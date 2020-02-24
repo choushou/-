3fcbc2237aaae9323153c9a2210175eb57b1ab42
張璋 <>
2020-02-24 19:42:43 +0900

    重力についてのサンプルを追加する

--- IOSRadarView-master版本2/IOSRadarView/RadarViewController.swift
+++ IOSRadarView-master版本2/IOSRadarView/RadarViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AudioToolbox
+import CoreMotion
 
 enum PointType {
     case body
@@ -27,7 +28,7 @@ class WineKind: NSObject {
     var windy: Float = 0.0
 }
 
-class RadarViewController: UIViewController {
+class RadarViewController: UIViewController,UIAccelerometerDelegate {
     
     var radarView: RadarView!
     var radarViewTwo: RadarView!
@@ -35,6 +36,13 @@ class RadarViewController: UIViewController {
     var wineKind: WineKind = WineKind()
     var beginPoint = CGPoint()
     var endPoint = CGPoint()
+    
+    
+      var ball:UIImageView!
+         var speedX:UIAccelerationValue = 0
+         var speedY:UIAccelerationValue = 0
+         var motionManager = CMMotionManager()
+      
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,11 +53,54 @@ class RadarViewController: UIViewController {
         wineKind.floral = 0.5
         wineKind.fruity = 0.25
         wineKind.windy = 0.75
-        
+  
         initSubviews()
+        
+//        //放一个小球在中央
+//              ball = UIImageView(image:UIImage(named:"ball"))
+//              ball.frame = CGRect(x:0, y:0, width:50, height:50)
+//              ball.center = self.view.center
+//              self.view.addSubview(ball)
+//
+//              motionManager.accelerometerUpdateInterval = 1/60
+//
+//              if motionManager.isAccelerometerAvailable {
+//                  let queue = OperationQueue.current
+//                  motionManager.startAccelerometerUpdates(to: queue!, withHandler: {
+//                      (accelerometerData, error) in
+//                          //动态设置小球位置
+//                          self.speedX += accelerometerData!.acceleration.x
+//                          self.speedY +=  accelerometerData!.acceleration.y
+//                          var posX=self.ball.center.x + CGFloat(self.speedX)
+//                          var posY=self.ball.center.y - CGFloat(self.speedY)
+//                          //碰到边框后的反弹处理
+//                          if posX<0 {
+//                              posX=0;
+//                              //碰到左边的边框后以0.4倍的速度反弹
+//                              self.speedX *= -0.4
+//
+//                          }else if posX > self.view.bounds.size.width {
+//                              posX=self.view.bounds.size.width
+//                              //碰到右边的边框后以0.4倍的速度反弹
+//                              self.speedX *= -0.4
+//                          }
+//                          if posY<0 {
+//                              posY=0
+//                              //碰到上面的边框不反弹
+//                              self.speedY=0
+//                          } else if posY>self.view.bounds.size.height{
+//                              posY=self.view.bounds.size.height
+//                              //碰到下面的边框以1.5倍的速度反弹
+//                              self.speedY *= -1.5
+//                          }
+//                      self.ball.center = CGPoint(x:posX, y:posY)
+//                  })
+//              }
+//
+        
     }
     
-    
+  
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch: AnyObject in touches{
             let point = (touch as AnyObject).location(in: self.radarView)
