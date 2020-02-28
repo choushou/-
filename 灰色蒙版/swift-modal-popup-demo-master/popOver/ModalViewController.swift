cbe7a6e44cd705dcf0e6f2360e78388736a0670d
張璋 <>
2020-02-28 13:33:43 +0900

    半透明の画面を実装する

--- 灰色蒙版/swift-modal-popup-demo-master/popOver/ModalViewController.swift
+++ 灰色蒙版/swift-modal-popup-demo-master/popOver/ModalViewController.swift
@@ -5,6 +5,6 @@ class ModalViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.6)
+        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.8)
     }
 }
