92cbced0692bbe31f3eee5923b156d78d8b04b95
張璋 <>
2020-03-21 14:22:43 +0900

    新規のプロジェクトを生成する

---
+++ beaverage/beaverage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  beaverage
+//
+//  Created by 張璋 on 2020/03/21.
+//  Copyright © 2020 sunasterisk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
